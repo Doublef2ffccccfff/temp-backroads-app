4fcababe33bf19022a3af450611c666efbe1da1f
Ankit Dixit <deus4200@gmail.com>
2023-12-02 11:26:31 +0530

    first repo

---
+++ src/components/About.js
@@ -0,0 +1,32 @@
+import Title from './Title';
+const About = () => {
+  const imgAbout = 'https://teara.govt.nz/files/32581-pc.jpg';
+  return (
+    <section className="section" id="about">
+      <Title title="about" subTitle="us" />
+
+      <div className="section-center about-center">
+        <div className="about-img">
+          <img src={imgAbout} className="about-photo" alt="awesome beach" />
+        </div>
+        <article className="about-info">
+          <h3>explore the difference</h3>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur
+            quisquam harum nam cumque temporibus explicabo dolorum sapiente odio
+            unde dolor?
+          </p>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur
+            quisquam harum nam cumque temporibus explicabo dolorum sapiente odio
+            unde dolor?
+          </p>
+          <a href="#about" className="btn">
+            read more
+          </a>
+        </article>
+      </div>
+    </section>
+  );
+};
+export default About;
